d8854dedebfd7bc7d27850e9ff4c1a149fa2e9a2
pzeus <pzeus@163.com>
2018-05-17 23:28:02 +0800

    youtu.sh

---
+++ youtu.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/pzeus/youtu/master/twts_26_Youtuber.zip
+unzip x twts_26_Youtuber.zip
+cd twts_26_Youtuber
+cp ./index.html /home/wwwroot/default/
+cd -rf ./assets /home/wwwroot/default/
+cd ..
+rm -fr twts_26_Youtuber
