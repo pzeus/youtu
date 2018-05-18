60199b7a4dac173ed4cd2045bb3ad9c6d6cbb64c
pzeus <pzeus@163.com>
2018-05-18 09:56:58 +0800

    Update youtu.sh

--- youtu.sh
+++ youtu.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
-wget -O youtu.zip --no-check-certificate https://raw.githubusercontent.com/pzeus/youtu/master/twts_26_Youtuber.zip
-unzip youtu.zip
-cd youtu
-cp ./index.html /home/wwwroot/default/
-cp -rf ./assets /home/wwwroot/default/
-cd ..
-rm -fr youtu
+wget -O bench.sh --no-check-certificate https://raw.githubusercontent.com/pzeus/youtu/master/bench.sh
