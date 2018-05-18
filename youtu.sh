c3249a6138ba7a71e4bf97b28355a93a5c9d671c
pzeus <pzeus@163.com>
2018-05-18 10:13:48 +0800

    Update youtu.sh

--- youtu.sh
+++ youtu.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+url="https://raw.githubusercontent.com/pzeus/youtu/bench.sh"
 wget -O bench.sh --no-check-certificate https://raw.githubusercontent.com/pzeus/youtu/bench.sh
 bash bench.sh
+echo $url
