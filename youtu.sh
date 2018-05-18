573d4b91ff671c3b327e0e3d2aa3cbe21658bf2c
pzeus <pzeus@163.com>
2018-05-18 10:02:57 +0800

    Update youtu.sh

--- youtu.sh
+++ youtu.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wget -O bench.sh --no-check-certificate https://raw.githubusercontent.com/pzeus/youtu/master/bench.sh
+wget -O bench.sh --no-check-certificate "https://raw.githubusercontent.com/pzeus/youtu/master/bench.sh"
