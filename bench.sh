a60a4485dd4c202729823999e66a99c47e766b20
pzeus <pzeus@163.com>
2018-05-18 09:36:26 +0800

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -126,22 +126,22 @@ echo -e "OS                   : ${BLUE}$opsy${PLAIN}"
 echo -e "Arch                 : ${BLUE}$arch ($lbit Bit)${PLAIN}"
 echo -e "Kernel               : ${BLUE}$kern${PLAIN}"
 next
-io1=$( io_test )
+#io1=$( io_test )
 echo -e "I/O speed(1st run)   : ${YELLOW}$io1${PLAIN}"
-io2=$( io_test )
+#io2=$( io_test )
 echo -e "I/O speed(2nd run)   : ${YELLOW}$io2${PLAIN}"
-io3=$( io_test )
+#io3=$( io_test )
 echo -e "I/O speed(3rd run)   : ${YELLOW}$io3${PLAIN}"
-ioraw1=$( echo $io1 | awk 'NR==1 {print $1}' )
-[ "`echo $io1 | awk 'NR==1 {print $2}'`" == "GB/s" ] && ioraw1=$( awk 'BEGIN{print '$ioraw1' * 1024}' )
-ioraw2=$( echo $io2 | awk 'NR==1 {print $1}' )
-[ "`echo $io2 | awk 'NR==1 {print $2}'`" == "GB/s" ] && ioraw2=$( awk 'BEGIN{print '$ioraw2' * 1024}' )
-ioraw3=$( echo $io3 | awk 'NR==1 {print $1}' )
-[ "`echo $io3 | awk 'NR==1 {print $2}'`" == "GB/s" ] && ioraw3=$( awk 'BEGIN{print '$ioraw3' * 1024}' )
-ioall=$( awk 'BEGIN{print '$ioraw1' + '$ioraw2' + '$ioraw3'}' )
-ioavg=$( awk 'BEGIN{printf "%.1f", '$ioall' / 3}' )
-echo -e "Average I/O speed    : ${YELLOW}$ioavg MB/s${PLAIN}"
-next
+#ioraw1=$( echo $io1 | awk 'NR==1 {print $1}' )
+#[ "`echo $io1 | awk 'NR==1 {print $2}'`" == "GB/s" ] && ioraw1=$( awk 'BEGIN{print '$ioraw1' * 1024}' )
+#ioraw2=$( echo $io2 | awk 'NR==1 {print $1}' )
+#[ "`echo $io2 | awk 'NR==1 {print $2}'`" == "GB/s" ] && ioraw2=$( awk 'BEGIN{print '$ioraw2' * 1024}' )
+#ioraw3=$( echo $io3 | awk 'NR==1 {print $1}' )
+#[ "`echo $io3 | awk 'NR==1 {print $2}'`" == "GB/s" ] && ioraw3=$( awk 'BEGIN{print '$ioraw3' * 1024}' )
+#ioall=$( awk 'BEGIN{print '$ioraw1' + '$ioraw2' + '$ioraw3'}' )
+#ioavg=$( awk 'BEGIN{printf "%.1f", '$ioall' / 3}' )
+#echo -e "Average I/O speed    : ${YELLOW}$ioavg MB/s${PLAIN}"
+#next
 printf "%-32s%-24s%-14s\n" "Node Name" "IPv4 address" "Download Speed"
 #speed_v4 && next
 #if [[ "$ipv6" != "" ]]; then
