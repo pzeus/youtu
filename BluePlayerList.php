79d3f3a47d0b6c7c176ffac32b23151f0c3653a8
pzeus <pzeus@163.com>
2018-07-10 22:21:58 +0800

    Update BluePlayerList.php

--- BluePlayerList.php
+++ BluePlayerList.php
@@ -9,15 +9,17 @@
 		player($_GET["playfile"]); 
 		echo "<a href=BluePlayerList.php>BACK</a>";
 	}
-
-  	//get dir
-  	$dir =  dirname(__FILE__);
-  	//get filelist
-  	$filelist = scandir($dir);
-  	//display filelist 
-	foreach($filelist as $key=>$value)
+	else
 	{
-		echo "<p><a href=BluePlayerList.php?status=play&playfile=".$value.">".$value."</a>";
-	}	
+		//get dir
+		$dir =  dirname(__FILE__);
+		//get filelist
+		$filelist = scandir($dir);
+		//display filelist 
+		foreach($filelist as $key=>$value)
+		{
+			echo "<p><a href=BluePlayerList.php?status=play&playfile=".$value.">".$value."</a>";
+		}
+	}
 ?>
 
