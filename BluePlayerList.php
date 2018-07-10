369cff4308c74968ae9e0b15454802913fb5e4e8
pzeus <pzeus@163.com>
2018-07-10 20:59:18 +0800

    Create BluePlayerList.php

---
+++ BluePlayerList.php
@@ -0,0 +1,11 @@
+<?php
+  //获取当前文件所在的绝对目录
+  $dir =  dirname(__FILE__);
+  //扫描文件夹
+  $filelist = scandir($dir);
+  //显示
+  foreach (array_expression as $key => $value)
+  {
+    echo "<p>".$value."</p>";
+  }
+?>
