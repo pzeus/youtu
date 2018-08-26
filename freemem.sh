f1a8b4ab6e5fd2edd001c4a2e3a8e900c0da18c2
pzeus <pzeus@163.com>
2018-08-26 21:13:55 +0800

    Create freemem.sh

---
+++ freemem.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#加入Linux Crontab 定时任务：
+# echo "00 00 * * * root /root/freemem.sh" >> /etc/crondtab
+
+used=`free -m | awk 'NR==2' | awk '{print $3}'`
+free=`free -m | awk 'NR==2' | awk '{print $4}'`
+
+echo "===========================" >> /var/log/mem.log
+date >> /var/log/mem.log
+echo "Memory usage | [Use：${used}MB][Free：${free}MB]" >> /var/log/mem.log
+
+if [ $free -le 100 ] ; then
+                sync && echo 1 > /proc/sys/vm/drop_caches
+                sync && echo 2 > /proc/sys/vm/drop_caches
+                sync && echo 3 > /proc/sys/vm/drop_caches
+                echo "OK" >> /var/log/mem.log
+else
+                echo "Not required" >> /var/log/mem.log
+fi
+exit
