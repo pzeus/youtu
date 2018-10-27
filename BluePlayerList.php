a660f2f0926a87c50c7880c9e6566eb299b7723e
pzeus <pzeus@163.com>
2018-10-27 21:07:47 +0800

    Update BluePlayerList.php

--- BluePlayerList.php
+++ BluePlayerList.php
@@ -3,15 +3,7 @@
 
 	function player($file,$width="70%",$height="70%") {
 		echo "<body bgcolor=black>";
-		echo "<script>";
-		echo "var video = document.getElementById('video');";
-		echo "function getPlaySpeed() {";"
-		echo "alert(video.playbackRate);//获取视频/音频的播放速度";
-		echo "}";
-		echo "function setPlaySpeed() {";
-		echo "video.playbackRate=2;//设置视频/音频的播放速度";
-		echo "}"
-		echo "</script>";
+
 		echo "<center><video id=myvideo src=".$file." width=".$width." height=".$height." controls></video></center>";
 		echo "<p><button onclick=setPlaySpeed() type=button>将视频设置为以快速播放</button></p>";
 		echo "</body>";
